5978b0e250a44c0999c1cb8317742aa0019bf7ff
vitali007tut <vitali007@tut.by>
2023-02-20 21:08:43 +0300

    feat: solution guessing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
+    min
+    max
+    middle
 
     setRange(min, max) {
-
+        new GuessingGame()
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.max - this.min) / 2) + this.min
+        return this.middle
     }
 
     lower() {
-
+        this.max = this.middle
     }
 
     greater() {
-
+        this.min = this.middle
     }
 }
 
